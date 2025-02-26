ed0ba348ec94b191f616ed9d1fd0b1bc94701bc5
Your Name <you@example.com>
2025-02-26 17:39:54 +0100

    temp_node

--- src/temp_node.cpp
+++ src/temp_node.cpp
@@ -13,15 +13,14 @@
 #include "version.h"
 #include "server.h"
 
-#ifndef __TEST__
-uint8_t mac[] =  {0xDE, 0xBB, 0x7E, 0xE1, 0xBA, 0x00};
-#else
-uint8_t mac[] =  {0xDE, 0xBB, 0x7E, 0xE1, 0xBB, 0x00};
-#endif
+//#define TEMP_NODE_NAME "AU"
 
-
+uint8_t mac[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};  // unused on esp32
 node_t node_info = {
-        "au", server, port, mac,
+        TEMP_NODE_NAME "AU",
+        server,
+        port,
+        mac,
 };
 
 const int num_switches = 0;
@@ -30,7 +29,9 @@ switch_t switches[num_switches] = {};
 const int num_temps = 1;
 const long period_t = 60000;
 const int ds18b_pin = 61;
-temp_t temps[num_temps] = {{"AU", 0, 0, 0, 0},};
+temp_t temps[num_temps] = {
+        {TEMP_NODE_NAME, 6, 0, 0, 0},
+};
 
 const int num_thermos = 0;
 thermos_t thermos[num_thermos] = {};
@@ -50,5 +51,4 @@ ntimer_t timers[num_timers] = {};
 const int num_pwms = 0;
 pwm_t pwms[num_pwms] = {};
 
-void user_logic() {
-}
+void user_logic() {}
