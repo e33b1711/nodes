c7d4729a57579e2b72be697e455d42f7c67cfb42
Your Name <you@example.com>
2025-02-26 15:27:58 +0100

    Fix logic after write_any

--- lib/node_lib/logic.cpp
+++ lib/node_lib/logic.cpp
@@ -1,7 +1,6 @@
 #include "node.h"
 #include "switch.h"
 #include "output.h"
-#include "rollo.h"
 
 // switch with long / short press
 void long_short(const int b_index,
@@ -10,9 +9,9 @@ void long_short(const int b_index,
                 const String l_short,
                 const int s_value) {
     if (switches[b_index].release_late)
-        write_output(l_long, l_value);
+        write_any(l_long, String(l_value));
     if (switches[b_index].release_early)
-        write_output(l_short, s_value);
+        write_any(l_short, String(s_value));
 }
 
 // switch with long / short press
@@ -30,7 +29,7 @@ void long_short_extern(const int b_index,
 // simple switch to output state
 void simple(const int b_index, const int value, const String l) {
     if (switches[b_index].press) {
-        write_rollo(l, value);
+        write_any(l, String(value));
     }
 }
 
@@ -42,12 +41,12 @@ void simple_extern(const int b_index, const int value, const String l) {
 
 // roolo switch click for complete up/down / hold and release for partial up/down
 void rollo_lock(const int b_index, const int value, const String l) {
-    if (switches[b_index].press){
-        write_rollo(l, value);
+    if (switches[b_index].press) {
+        write_any(l, String(value));
         Serial.println("DEBUG: rollo_lock press: " + l + " " + String(value));
     }
-    if (switches[b_index].release_late){
-        write_rollo(l, 50);
+    if (switches[b_index].release_late) {
+        write_any(l, String(50));
         Serial.println("DEBUG: rollo_lock release_late: " + l + " " + 50);
     }
 }
@@ -60,7 +59,7 @@ void toggle_couple(const String output) {
         return;
     }
     send_command(output, not(value));
-    write_output(output, not(value));
+    write_any(output, String(not(value)));
 }
 
 // toggle a output that lives here and on another unit on press
