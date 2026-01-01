6746d860b9d27c4895411e1b9eb92cb1208aa2be
Your Name <you@example.com>
2026-01-01 13:57:32 +0100

    Add temps to ha config

--- ha_mqtt.py
+++ ha_mqtt.py
@@ -31,6 +31,30 @@ def ro_format(item):
 #TODO rollos stop => node side (more state updates)
 #TI / HI V TS revision
 
+def sensor_format(item, unit = "°C", precision = 0.5):  
+    str = f'''
+  - unique_id: "{item}"
+    name: "{item}"
+    state_topic: "ard_state/{item}"
+    suggested_display_precision: {precision}
+    unit_of_measurement: "{unit}"
+    '''
+    return str
+
+def number_format(item, unit = "", min = 0, max = 255):  
+    str = f'''
+  - unique_id: "{item}"
+    name: "{item}"
+    command_topic: "ard_command/{item}"
+    state_topic: "ard_state/{item}"
+    unit_of_measurement: "{unit}"
+    min: "{min}"
+    max: "{max}"
+    optimistic: false
+    '''
+    return str
+
+
 light_items = [
 'LI_EG_WZ',	 
 'LI_EG_WZ_L1',
@@ -97,12 +121,91 @@ ro_items = [
 "DO_CH"]
 
 
+sensor_items = [
+"TI_PU_O ",
+"TI_PU_U ",
+"F_HE",
+"F_WW",
+"F_RAIN",
+"TI_EG_WZ",
+"TI_EG_KU",
+"TI_EG_EZ",
+"TI_EG_GA",
+"TI_OG_BA",
+"TI_OG_KN",
+"TI_OG_KS",
+"TI_OG_SZ",
+"TI_OG_GA",
+"TI_UG_HO",
+"TI_UG_WK",
+"TI_UG_HK",
+"TI_UG_GA",
+"TI_UG_LA",
+"TI_GR",
+"TI_AU",
+"TI_CH", 	   
+"HI_EG_WZ",
+"HI_EG_KU",
+"HI_EG_EZ",
+"HI_EG_GA",
+"HI_OG_BA",
+"HI_OG_KN",
+"HI_OG_KS",
+"HI_OG_SZ",
+"HI_OG_GA",
+"HI_UG_HO",
+"HI_UG_WK",
+"HI_UG_HK",
+"HI_UG_GA",
+"HI_UG_LA",
+"HI_GR",
+"HI_AU",
+"HI_CH",
+"V_UG_HO",
+"V_UG_LA",
+"V_UG_GA",
+"V_UG_WK",
+"V_EG_KU",
+"V_EG_EZ",
+"V_EG_GA",
+"V_EG_WZ",
+"V_EG_GR",
+"V_EG_WC",
+"V_OG_KS",
+"V_OG_KN",
+"V_OG_GA",
+"V_OG_BA",
+"V_OG_SZ",
+]
+
+#TODO unfy U_ + V_
+
+number_items = [
+"U_EL",
+"TS_UG_HO",     
+"TS_UG_LA",     
+"TS_UG_GA",     
+"TS_UG_WK",     
+"TS_EG_KU",     
+"TS_EG_EZ",     
+"TS_EG_GA",     
+"TS_EG_WZ",     
+"TS_EG_GR",     
+"TS_EG_WC",     
+"TS_OG_KS",     
+"TS_OG_KN",     
+"TS_OG_GA",     
+"TS_OG_BA",     
+"TS_OG_SZ",
+]
+
+
 
 
 
 if __name__ == "__main__":
 
-    with open("mqtt.yaml", "w") as fh:
+    with open("mqtt.yaml", "w", encoding="utf-8") as fh:
         fh.write("- light:\n")
         for item in light_items:
             fh.write(l_format(item))
@@ -111,24 +214,18 @@ if __name__ == "__main__":
         for item in ro_items:
             fh.write(ro_format(item))
 
-
-
-#- sensor:
-#  - unique_id: "TI_OG_GA"
-#    name: "TI_OG_GA"
-#    state_topic: "ard_state/TI_OG_GA"
-#    suggested_display_precision: 1
-#    unit_of_measurement: "°C"
-#
-#  - unique_id: "HI_OG_GA"
-#    name: "HI_OG_GA"
-#    state_topic: "ard_state/HI_OG_GA"
-#    unit_of_measurement: "%"
-#
-#- number:
-#  - unique_id: "TS_OG_GA"
-#    name: "TS_OG_GA"
-#    state_topic: "ard_state/TS_OG_GA"
-#    command_topic: "ard_command/TS_OG_GA"
-#    unit_of_measurement: "°C"
-#    optimistic: false
+        fh.write("\n\n- sensor:\n")
+        for item in sensor_items:
+            if item.startswith("TI_"):
+                fh.write(sensor_format(item, "°C", 0.5))
+            elif item.startswith("HI_"):
+                fh.write(sensor_format(item, "%", 1))
+            else:
+                fh.write(sensor_format(item, "", 1))
+        
+        fh.write("\n\n- number:\n")
+        for item in number_items:
+            if item.startswith("TS_"):
+                fh.write(number_format(item, "°C", 10, 30))
+            else:
+                fh.write(number_format(item, "", 0, 255))
